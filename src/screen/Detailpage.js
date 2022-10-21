8c10c66fc52d2844947a89639c8ea7ef3539baaa
arbayu26 <you@example.com>
2022-10-21 21:19:14 +0700

    process 1

---
+++ src/screen/Detailpage.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Detailpage = () => {
+  return (
+    <View>
+      <Text>Detailpage</Text>
+    </View>
+  )
+}
+
+export default Detailpage
